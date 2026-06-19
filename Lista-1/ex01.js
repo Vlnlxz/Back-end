253d8436ac30b49a3b7a6cd700e24c80a81651a1
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-06-19 11:30:34 -0300

    Pronto

--- Lista-1/ex01.js
+++ Lista-1/ex01.js
@@ -1,1 +1,6 @@
-asssa
+const input = require('readline-sync')
+
+let celsius = input.questionFloat("Digite o valor em celsius: ");
+let fahrenheit = celsius * 1.8 + 32
+
+console.log(`A temperatura em ${celsius} C convertido é de ${fahrenheit} F`);
