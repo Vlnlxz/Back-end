04b1c80f9d984da4a73f5dbb341b3fa0c1e9e95d
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-06-19 12:18:39 -0300

    Fazendo

---
+++ Lista-1/ex03.js
@@ -0,0 +1,6 @@
+const input = require('readline-sync')
+
+let numero = Number(input.question("Digite um numero: ");
+let resultado = n %2===2?"Par" : "Impar"
+
+console.log(`O numero digitado é ${resultado}`);
