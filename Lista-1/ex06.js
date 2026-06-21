6492d3b0fe74daab830efdbb87f3ea9f7e66d424
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-06-20 21:49:35 -0300

    Pronto

---
+++ Lista-1/ex06.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync')
+
+let numero = Number(input.question("Digite um numero: "));
+for (let i = numero; i >= 1; i--) {
+    console.log(i); } 
+
+    console.log("Fim")
