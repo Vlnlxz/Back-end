0ee74eeabe21f5772cc67341269fb21ad461e8cf
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-06-21 21:45:38 -0300

    Pronto

---
+++ Lista-1/ex09.js
@@ -0,0 +1,10 @@
+//Exercício 9 – Mínimo. Escreva uma função min(a, b) que receba dois números e retorne o menor deles.
+//Não use Math.min. Escreva a versão tradicional e depois reescreva como Arrow Function.
+
+function min(a,b) {
+    return (a < b) ? a : b
+}
+
+const minArrow = (a, b) => (a < b) ? a : b;
+console.log(min(5, 3));
+console.log(minArrow(5, 3));
