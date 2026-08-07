7a28e0ecb3283c072080d78639e599d29ead134f
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-08-07 10:56:05 +0000

    Pronto

--- Aula-05/ex-01.js
+++ Aula-05/ex-01.js
@@ -9,18 +9,23 @@ res.json({ mensagem: " Meu servidor esta no ar!" });
 });
 
 const produtos = [
-    { id: 1, nome: "Teclado", preço: 250 },
-    { id: 2, nome: "Mouse", preço: 150 },
-    { id: 3, nome: "Monitor", preço: 450 },
-    { id: 4, nome: "Cadeira", preço: 400 }
+    { id: 1, nome: "Teclado", preco: 250 },
+    { id: 2, nome: "Mouse", preco: 150 },
+    { id: 3, nome: "Monitor", preco: 450 },
+    { id: 4, nome: "Cadeira", preco: 400 }
 ]
 app.get('/produtos', (req , res)=>{
     res.json(produtos);
 });
 
-res.get('/status', (req , res)=> {
-    res.status(200).json({online:true, mensagem: "API funcionando"});
-}
+app.get('/status', (req , res)=> {
+    res.status(200).json({online:"true", mensagem: "API funcionando"});
+})
+
+app.get('/produtos/caros', (req, res) => {
+    const produtoscaros = produtos.filter(p => p.preco > 100);
+    res.json(produtoscaros);
+})
 
 app.listen(3000, () => {
 console.log(" Servidor rodando em http://localhost:3000 ");
